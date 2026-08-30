138cf8b4471e55640fdec50fd73f0cc120aa4176
KingBrezzz <bobnbb046@gmail.com>
2026-08-30 20:11:55 +0700

    Refactor AegisAC for improved readability and structure

--- src/main/java/id/kingbrezz/aegisac/AegisAC.java
+++ src/main/java/id/kingbrezz/aegisac/AegisAC.java
@@ -1,5 +1,11 @@
 package id.kingbrezz.aegisac;
 
+import id.kingbrezz.aegisac.alert.AlertManager;
+import id.kingbrezz.aegisac.check.CheckManager;
+import id.kingbrezz.aegisac.command.AegisCommand;
+import id.kingbrezz.aegisac.manager.ConfigManager;
+import id.kingbrezz.aegisac.manager.MessageManager;
+import id.kingbrezz.aegisac.player.PlayerDataManager;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +15,12 @@ public final class AegisAC extends JavaPlugin {
 
     private static AegisAC instance;
 
+    private ConfigManager configManager;
+    private MessageManager messageManager;
+    private PlayerDataManager playerDataManager;
+    private CheckManager checkManager;
+    private AlertManager alertManager;
+
     @Override
     public void onEnable() {
         instance = this;
@@ -16,19 +28,46 @@ public final class AegisAC extends JavaPlugin {
         saveDefaultConfig();
         saveResource("messages.yml", false);
 
-        getLogger().info("AegisAC v" + getDescription().getVersion() + " is starting...");
+        getLogger().info(
+                "AegisAC v"
+                        + getDescription().getVersion()
+                        + " is starting..."
+        );
+
         getLogger().info("Author: KingBrezz");
         getLogger().info("Platform: Paper");
         getLogger().info("Target: Minecraft Java Edition");
 
+        /*
+         * Core managers
+         */
+        configManager = new ConfigManager(this);
+        messageManager = new MessageManager(this);
+        playerDataManager = new PlayerDataManager(this);
+        checkManager = new CheckManager(this);
+
+        /*
+         * Alert system
+         */
+        alertManager = new AlertManager(
+                this
+        );
+
+        /*
+         * Commands
+         */
         registerCommands();
 
-        getLogger().info("AegisAC has been enabled.");
+        getLogger().info(
+                "AegisAC has been enabled."
+        );
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("AegisAC has been disabled.");
+        getLogger().info(
+                "AegisAC has been disabled."
+        );
 
         if (instance == this) {
             instance = null;
@@ -36,27 +75,56 @@ public final class AegisAC extends JavaPlugin {
     }
 
     private void registerCommands() {
-        PluginCommand command = getCommand("aegisac");
+        PluginCommand command =
+                getCommand("aegisac");
 
         if (command == null) {
             getLogger().log(
                     Level.SEVERE,
-                    "Unable to register /aegisac. The command is missing from plugin.yml."
+                    "Unable to register /aegisac. "
+                            + "The command is missing "
+                            + "from plugin.yml."
             );
             return;
         }
 
-        /*
-         * The command executor will be attached when the command
-         * module is added. Keeping registration isolated here
-         * prevents command setup from being scattered across the
-         * plugin lifecycle.
-         */
+        AegisCommand executor =
+                new AegisCommand(
+                        this,
+                        messageManager,
+                        checkManager,
+                        playerDataManager
+                );
+
+        command.setExecutor(executor);
+        command.setTabCompleter(executor);
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public CheckManager getCheckManager() {
+        return checkManager;
+    }
+
+    public AlertManager getAlertManager() {
+        return alertManager;
     }
 
     public static AegisAC getInstance() {
         if (instance == null) {
-            throw new IllegalStateException("AegisAC is not enabled.");
+            throw new IllegalStateException(
+                    "AegisAC is not enabled."
+            );
         }
 
         return instance;
